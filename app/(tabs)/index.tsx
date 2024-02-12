a2e2de2f50ef028e0b4956f4cf90a67fc21900be
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2024-02-12 17:33:22 -0500

    initial setup for firebase

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,12 +3,50 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
 
+import { useState, useEffect } from 'react'
+import { db } from "../data/firebaseConfig"
+import { getDocs, collection } from 'firebase/firestore';
+
 export default function TabOneScreen() {
+  const [userList, setUserList] = useState<{id: string; [key: string]: any; }[]>([]);
+  const usersCollectionRef = collection(db, "users")
+
+  useEffect(() => {
+    const getUsersList = async () => {
+      try {
+        const data = await getDocs(usersCollectionRef);
+        const filteredData = data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id
+        }));
+        setUserList(filteredData);
+        console.log(filteredData);
+      }
+      catch (err) {
+        console.log(err);
+      }
+    }
+
+    getUsersList();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+
+      {userList.map((user) => 
+        <Text style={styles.title}> 
+          <Text style={styles.title}> 
+            username: {user.username}
+          </Text> 
+          <Text style={styles.title}> 
+            {'\n'}
+            dateJoined: {user.dateJoined} 
+          </Text>
+        </Text>
+        
+      )}
+      
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
