04197616883d663f6b0d0633e0c75d42d5ccf4eb
Junyi Huang <jyiyi@bu.edu>
2024-02-25 17:26:30 -0500

    styling update

--- app/(tabs)/deckprev.tsx
+++ app/(tabs)/deckprev.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, Text, FlatList, TouchableOpacity } from 'react-native';
-import { MaterialCommunityIcons, MaterialIcons, Octicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons, MaterialIcons, AntDesign } from '@expo/vector-icons';
 
 interface WordItem {
   id: string;
@@ -40,39 +40,41 @@ const dummyData: WordItem[] = [
 export default function DeckPreview() {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <TouchableOpacity>
-          <MaterialIcons name="arrow-back-ios" size={24} color="black"/>
-        </TouchableOpacity>
-        <Text style={styles.headerTitle}>Someone’s Study Set</Text>
-        <View style={styles.subHeader}>
-          <MaterialCommunityIcons name="cards-variant" size={18} color="gray"/>
-          <Text style={styles.wordCount}>{dummyData.length} words</Text>
+        <View style={styles.header}>
+            <TouchableOpacity style={styles.backIcon}>
+                <MaterialIcons name="arrow-back-ios" size={24} color="gray"/>
+            </TouchableOpacity>
+            <View style={styles.headerContent}>
+                <Text style={styles.headerTitle}>Someone’s Study Set</Text>
+                <View style={styles.subHeader}>
+                    <MaterialCommunityIcons name="cards-variant" size={18} color="gray"/>
+                    <Text style={styles.wordCount}>{dummyData.length} words</Text>
+                </View>
+            </View>
         </View>
-      </View>
-    
-      <FlatList
+        
+    <FlatList
         data={dummyData}
         renderItem={({ item }) => (
         <TouchableOpacity>
-          <View style={styles.listItem}>
-            <View style={styles.previewBadge}>
-              <Text style={styles.previewBadgeText}>{item.term[0]}</Text>
+            <View style={styles.listItem}>
+                <View style={styles.previewBadge}>
+                    <Text style={styles.previewBadgeText}>{item.term[0]}</Text>
+                </View>
+                <View style={styles.termContainer}>
+                    <Text style={styles.term}>{item.term}</Text>
+                    <Text style={styles.definition}>{item.definition}</Text>
+                </View>
+                <AntDesign name="play" size={36} color="#FFDF37"/>
             </View>
-            <View style={styles.termContainer}>
-              <Text style={styles.term}>{item.term}</Text>
-              <Text style={styles.definition}>{item.definition}</Text>
-            </View>
-              <Octicons name="dot-fill" size={48} color="gray"/>
-          </View>
         </TouchableOpacity>
         )}
         keyExtractor={item => item.id}
       />
       
-      <TouchableOpacity style={styles.practiceButton}>
-        <Text style={styles.practiceButtonText}>Practice</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.practiceButton}>
+            <Text style={styles.practiceButtonText}>Practice</Text>
+        </TouchableOpacity>
     </View>
   );
 };
@@ -81,22 +83,26 @@ export default function DeckPreview() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f0f0f0',
+    backgroundColor: '#F2E8E1',
   },
   header: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'flex-start',
     padding: 20,
-    backgroundColor: 'white',
+    backgroundColor: '#F2E8E1',
   },
   backIcon: {
-    width: 24,
-    height: 24,
+
+  },
+  headerContent: {
+    flexDirection: 'column',
+    justifyContent: 'center',
   },
   headerTitle: {
     fontSize: 20,
     fontWeight: 'bold',
+    marginBottom: 4,
   },
   subHeader: {
     flexDirection: 'row',
@@ -114,8 +120,10 @@ const styles = StyleSheet.create({
     marginVertical: 5, 
     marginHorizontal: 5,
     borderWidth: 1,
+    borderRightWidth: 4,
+    borderBottomWidth: 4,
     borderRadius: 10,
-    borderColor: 'white',
+    borderColor: '#000000',
     backgroundColor: 'white',
   },
   termContainer: {
@@ -134,20 +142,23 @@ const styles = StyleSheet.create({
     height: 48, 
     borderRadius: 10, 
     marginRight: 10,
-    backgroundColor: '#dddddd',
+    backgroundColor: '#7F9CEF',
     alignItems: 'center',
     justifyContent: 'center',
   },
   previewBadgeText: {
     fontSize: 20,
-    color: 'black',
-    fontWeight: 'bold', 
+    color: 'white', 
   },
   practiceButton: {
     margin: 20,
     padding: 20,
+    borderWidth: 1,
+    borderBottomWidth: 4,
+    borderRightWidth: 4,
     borderRadius: 10,
-    backgroundColor: 'white',
+    borderColor: '#000000',
+    backgroundColor: '#FFDF37',
     alignItems: 'center',
     justifyContent: 'center',
   },
