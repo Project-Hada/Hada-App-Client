a5ec68df539fdb85d824cb21ebb45d2bdaea8976
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2024-02-27 02:32:50 -0500

    add the function to add a user to database using a custom schema

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,21 @@
-import { StyleSheet } from 'react-native';
+import { Button, ScrollView, StyleSheet, TextInput } from 'react-native';
 
 import { Text, View } from '@/components/Themed';
 
 import { useState, useEffect } from 'react'
 import { db } from "../data/firebaseConfig"
-import { doc, getDoc, getDocs, collection, DocumentReference, DocumentData } from 'firebase/firestore';
+import { doc, getDoc, getDocs, collection, query, where, DocumentReference, DocumentData, addDoc } from 'firebase/firestore';
+
+import { UserSchema, userConverter } from '../collection/UserSchema'
 
 export default function TabOneScreen() {
   const [deckList, setDeckList] = useState<{id: string; [key: string]: any; }[]>([]);
   const [userList, setUserList] = useState<{id: string; [key: string]: any; }[]>([]);
   const usersCollectionRef = collection(db, "users")
   const decksCollectionRef = collection(db, "decks")
+
+  // for input form
+  const [text, onChangeText] = useState('');
 
   useEffect(() => {
     const getUsersList = async () => {
@@ -28,7 +33,7 @@ export default function TabOneScreen() {
         console.log(err);
       }
     }
-
+    
     getUsersList();
   }, [])
 
@@ -38,11 +43,11 @@ export default function TabOneScreen() {
         const data = await getDocs(decksCollectionRef);
         const filteredData = data.docs.map((doc) => ({
           ...doc.data(),
-          id: doc.id
+          id: doc.id,
         }));
 
         setDeckList(filteredData);
-        // console.log("users\n", filteredData);
+        console.log("decks\n", filteredData);
       }
       catch (err) {
         console.log(err);
@@ -52,8 +57,34 @@ export default function TabOneScreen() {
     getDeckList();
   }, [userList])
 
+
+
+  const handleSubmit = async () => {
+    console.log(text);
+    const dateObj = new Date();
+    const year = dateObj.getFullYear()
+    const month = dateObj.getMonth()
+    const day = dateObj.getDate()
+
+    const docRef = await addDoc(
+      usersCollectionRef.withConverter(userConverter), 
+      new UserSchema(text, String(`${month}/${day}/${year}`), [])
+    );
+    console.log("Document written with ID: ", docRef.id);
+  }
+
   return (
-    <View style={styles.container}>
+    <ScrollView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        placeholder='username'
+        value={text}
+      />
+      <Button
+        title="Submit"
+        onPress={handleSubmit}
+      />
 
       <Text><h1>Users</h1></Text>
       {userList.map((user) => 
@@ -67,22 +98,19 @@ export default function TabOneScreen() {
 
       <Text><h1>Decks</h1></Text>
       {deckList.map((deck) => 
-        <Text key={deck.id}> fi
+        <Text key={deck.id}>
           deck name: {deck.deckName}
           {'\n'}
-          
           <h3>cards:</h3>
           {deck.cards.map((c : any) => {
-              return (
-                  "front: " + c.front + "\nback: " + c.back + "\n-------------------\n"
-              )
+              return ( "front: " + c.front + "\nback: " + c.back + "\n-----------------\n" ) 
             })
           }
         </Text>
       )}
       
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-    </View>
+    </ScrollView>
   );
 }
 
@@ -101,4 +129,9 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  input: {
+    height: 40, 
+    borderColor: 'gray', 
+    borderWidth: 1,
+  }
 });
