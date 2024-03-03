eb5c25b8d5a9d41522cacb8c041b860a86eb480c
Junyi Huang <jyiyi@bu.edu>
2024-03-03 01:52:19 -0500

    styles

---
+++ styles/Colors.ts
@@ -0,0 +1,11 @@
+
+export const colors = {
+    blueSquare: '#F7FCEF',
+    yellowAccent: '#FFD747',
+    progressBarBlue: '#38DAEF',
+    redButton: '#F7454C',
+    greenButton: '#72DA4F',
+    background: '#F2E8E1',
+    text: '#000000',
+    subtext: '#A7A7A7',
+  };
---
+++ styles/Typography.ts
@@ -0,0 +1,39 @@
+
+export const typography = {
+    libraryTitle: {
+      fontFamily: 'GeneralSans-Bold',
+      fontSize: 24
+    },
+    setTitle: {
+      fontFamily: 'GeneralSans-Semibold',
+      fontSize: 20
+    },
+    subtext: {
+      fontFamily: 'GeneralSans-Regular',
+      fontSize: 14
+    },
+    deckTitle: {
+        fontFamily: 'GeneralSans-Semibold',
+        fontSize: 20
+    },
+    wordCount: {
+        fontFamily: 'GeneralSans-Regular',
+        fontSize: 14
+      },
+    koreanWord: {
+        fontFamily: 'GeneralSans-Medium',
+        fontSize: 22
+    },
+    engWord: {
+        fontFamily: 'GeneralSans-Regular',
+        fontSize: 16
+    },
+    cardKoreanWord: {
+        fontFamily: 'GeneralSans-SemiBold',
+        fontSize: 30
+    },
+    romanization: {
+        fontFamily: 'GeneralSans-Regular',
+        fontSize: 24
+    }
+  };
