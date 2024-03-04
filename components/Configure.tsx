06c69ab40b6b85a800f520a728475b5655e04ec1
Christian Rudder <ringthebellpepper@gmail.com>
2024-03-04 09:46:04 -0500

    styles reconfigured

--- components/Configure.tsx
+++ components/Configure.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import Router from "./Router";
 import { useFonts } from "expo-font";
 import { FontAwesome } from "@expo/vector-icons";
