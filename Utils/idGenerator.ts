0d54fec6c215eb626c9a36e54c7d5e2d84879a68
Christian Rudder <74576449+neezacoto@users.noreply.github.com>
2024-03-06 10:42:53 -0500

    Delete Utils directory

--- Utils/idGenerator.ts
+++
@@ -1,5 +0,0 @@
-function generateId() {
-    return `${Date.now()}-${Math.random().toString(36).substr(2, 9)}`;
-  }
-  
-export default generateId;
--- Utils/tts.ts
+++
@@ -1,9 +0,0 @@
-import * as Speech from 'expo-speech';
-
-const speak = (text: string, language: string = 'ko-KR') => {
-    Speech.speak(text, {
-        language, 
-    });
-};
-
-export default speak;
