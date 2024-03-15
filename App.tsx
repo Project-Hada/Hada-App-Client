4ca5a9db8d3175574f0d6e5273ff5dbf27cab359
Junyi Huang <jyiyi@bu.edu>
2024-03-15 15:56:44 -0400

    silent mode bug fix

--- App.tsx
+++ App.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Configure from "./components/Configure";
 import { LibraryProvider } from "./utils/contexts/LibraryContext";
+import * as Speech from "expo-speech";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import { Audio } from "expo-av";
+
+const soundObject = new Audio.Sound();
 
 export default function App() {
+  useEffect(() => {
+    const enableSound = async () => {
+      if (Platform.OS === "ios") {
+        await Audio.setAudioModeAsync({
+          playsInSilentModeIOS: true,
+        });
+        await soundObject.loadAsync(require("./soundFile.mp3"));
+        await soundObject.playAsync();
+      }
+    };
+    enableSound();
+  });
+
   return (
     <LibraryProvider>
       <Configure />
