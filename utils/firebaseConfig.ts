043eb6faeb668519ca6cb9ce285b1cee4bf74468
wilsonz0 <105890511+wilsonz0@users.noreply.github.com>
2024-03-17 22:26:56 -0400

    add the auth variable referring to the user authentication

--- utils/firebaseConfig.ts
+++ utils/firebaseConfig.ts
@@ -4,6 +4,7 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -23,3 +24,4 @@ export const app = initializeApp(firebaseConfig);
 const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
