393946848620a08db79ac570c6468157afd4ec67
wilsonz0 <wilsonzhang1313@gmail.com>
2024-03-29 16:11:44 -0400

    add the LoginScreen and the RegisterScreen

---
+++ components/pages/UserAuth/RegisterScreen.tsx
@@ -0,0 +1,92 @@
+import { AntDesign } from '@expo/vector-icons';
+import React from 'react';
+import { useState, useEffect } from 'react'
+import { Button, View, ScrollView, StyleSheet, TextInput, Pressable } from 'react-native';
+import { auth } from '../../../utils/firebaseConfig';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { addNewUserWithID } from '../../../utils/services/usersFunctions';
+
+export function RegisterScreen({ navigation, route }: any) {
+  const [usernameInput, setUsernameInput] = useState(''); 
+  const [emailInput, setEmailInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
+  const [confirmPasswordInput, setConfirmPasswordInput] = useState('');
+
+  const [error, setError] = useState('');
+
+  const handleSubmitForRegister = async () => {
+    try {
+      if (passwordInput != confirmPasswordInput) throw new Error("password mismatch")
+
+      createUserWithEmailAndPassword(auth, emailInput, passwordInput).then(response => {
+        console.log(response);
+        addNewUserWithID(response.user.uid, usernameInput);
+      });
+      
+      navigation.navigate("LibraryScreen")
+    }
+    catch (error) {
+      console.log(error)
+      if (error instanceof Error) {
+        setError("Passwords are mismatched. Try again.")
+      }
+      else {
+        setError("Something went wrong. Try again.")
+      }
+    }
+  }
+
+  return (
+    <View style={{paddingHorizontal: 20}}>
+      <h1> REGISTER </h1>
+
+      {(error !== "") && <p>{error}</p>}
+
+      {/* Custom Login */}
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsernameInput}
+        placeholder='username'
+        placeholderTextColor="#D3D3D3" 
+        value={usernameInput}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmailInput}
+        placeholder='email'
+        placeholderTextColor="#D3D3D3" 
+        value={emailInput}
+      />
+      <TextInput
+        secureTextEntry={true}
+        style={styles.input}
+        onChangeText={setPasswordInput}
+        placeholder='password'
+        placeholderTextColor="#D3D3D3" 
+        value={passwordInput}
+      />
+      <TextInput
+        secureTextEntry={true}
+        style={styles.input}
+        onChangeText={setConfirmPasswordInput}
+        placeholder='confirm your password'
+        placeholderTextColor="#D3D3D3" 
+        value={confirmPasswordInput}
+      />
+
+      <Button
+        title="Register"
+        onPress={handleSubmitForRegister}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40, 
+    borderColor: 'gray', 
+    borderWidth: 1,
+    backgroundColor: "white"
+  }
+})
