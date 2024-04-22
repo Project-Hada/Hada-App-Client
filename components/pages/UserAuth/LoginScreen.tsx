1ca6f913285ee48d751b48e67b1ec3bf9f57e8d2
SPC-Toad <sangyunkiment@gmail.com>
2024-04-22 00:12:24 -0400

    login-reg-UI

--- components/pages/UserAuth/LoginScreen.tsx
+++ components/pages/UserAuth/LoginScreen.tsx
@@ -6,17 +6,22 @@ import { auth } from '../../../utils/firebaseConfig';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import LibraryContext from '../../../utils/contexts/LibraryContext';
 
+import { useTheme } from "../../../utils/contexts/ThemeContext";
+
+
+
 export function LoginScreen({ navigation, route }: any) {
   const [emailInput, setEmailInput] = useState('');
   const [passwordInput, setPasswordInput] = useState('');
-
+  
   const [error, setError] = useState('');
-
+  
   const { user } = useContext(LibraryContext);
   useEffect(() => { 
-    if (user) navigation.navigate("LibraryScreen")
+    if (user) 
+      navigation.navigate("LibraryScreen")
   }, [user]);
-
+  
   const handleSubmitForLogin = async () => {
     try {
       const response = await signInWithEmailAndPassword(auth, emailInput, passwordInput)
@@ -29,47 +34,122 @@ export function LoginScreen({ navigation, route }: any) {
     }
   }
 
+  const {theme} = useTheme();
+  const loginStyle = StyleSheet.create({
+    mainContainer: {
+      width: "100%",
+      height: "100%",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: theme.colors.backgroundColor
+    },
+    loginContainer: {
+      width:'90%',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#FFFFFF',
+      borderWidth: 1,
+      borderRightWidth: 4,
+      borderBottomWidth: 4,
+      borderRadius: 20,
+      borderColor: theme.colors.border,
+    },
+    loginTitle: {
+      fontSize: 24,
+      fontWeight: "bold",
+      marginTop: 40,
+      marginBottom: 15,
+    },
+    loginInput: {
+      width: '80%',
+      fontSize: 16,
+      borderBottomWidth: 1,
+      borderStyle: 'solid',
+      borderBottomColor: 'black',
+      marginTop: 25,
+    },
+    loginForgotContainer: {
+      width: '80%',
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      fontSize: 11,
+      marginTop: 10,
+      marginBottom: 30,
+    },
+    loginForgotText: {
+      textDecorationLine: 'underline',
+      fontSize: 11,
+    },
+    loginRegisterContainer: {
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 40,
+    },
+    loginRegisterText: {
+      fontSize: 13,
+    },
+    loginRegisterLink: {
+      textDecorationLine: 'underline',
+      fontSize: 13,
+      fontWeight: "bold",
+    },
+    loginSignInBtn: {
+      marginBottom: 40,
+      fontSize: 16,
+      fontWeight: "bold",
+      color: '#FFFFFF',
+      backgroundColor: 'black',
+      paddingHorizontal: 70,
+      paddingVertical: 15, 
+      borderRadius: 11,
+    },
+  })
+  
   return (
-    <View style={{paddingHorizontal: 20}}>
-      <Text> LOGIN </Text>
+    <View style={loginStyle.mainContainer}>
+      <View style={loginStyle.loginContainer}>
+        <Text style={loginStyle.loginTitle}> LOGIN </Text>
 
-      {(error !== "") && <Text>{error}</Text>}
+        {(error !== "") && <Text>{error}</Text>}
 
-      {/* Custom Login */}
-      <TextInput
-        style={styles.input}
-        onChangeText={setEmailInput}
-        placeholder='email'
-        placeholderTextColor="#D3D3D3" 
-        value={emailInput}
-      />
-      <TextInput
-        secureTextEntry={true}
-        style={styles.input}
-        onChangeText={setPasswordInput}
-        placeholder='password'
-        placeholderTextColor="#D3D3D3" 
-        value={passwordInput}
-      />
+        {/* Custom Login */}
+        <TextInput
+          style={loginStyle.loginInput}
+          onChangeText={setEmailInput}
+          placeholder='Username'
+          placeholderTextColor="#D3D3D3" 
+          value={emailInput}
+        />
 
-      <Button
-        title="Login"
-        onPress={handleSubmitForLogin}
-      />
-      
-      {/* Custom Register */}
-      <Pressable style={{ flexDirection: 'row', paddingTop: 20}} onPress={() => navigation.navigate("RegisterScreen")}>
-        <AntDesign name="plussquareo" size={24} color="black"><Text>Register</Text></AntDesign>
-      </Pressable>
+        <TextInput
+          secureTextEntry={true}
+          style={loginStyle.loginInput}
+          onChangeText={setPasswordInput}
+          placeholder='Password'
+          placeholderTextColor="#D3D3D3" 
+          value={passwordInput}
+        />
+
+        <View style={loginStyle.loginForgotContainer}>
+          <Pressable >
+            <Text style={loginStyle.loginForgotText}>Forgot Password</Text>
+          </Pressable>
+        </View>
+
+        {/* Custom Register */}
+        <Pressable style={loginStyle.loginRegisterContainer} onPress={() => navigation.navigate("RegisterScreen")}>
+            <Text style={loginStyle.loginRegisterText}>Don't have an account?&nbsp;</Text>
+            <Text style={loginStyle.loginRegisterLink}>Register Here</Text>
+        </Pressable>
+
+        <Pressable onPress={handleSubmitForLogin}>
+          <Text style={loginStyle.loginSignInBtn}>Sign In</Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  input: {
-    height: 40, 
-    borderColor: 'gray', 
-    borderWidth: 1,
-    backgroundColor: "white"
-  }
-})
--- components/pages/UserAuth/RegisterScreen.tsx
+++ components/pages/UserAuth/RegisterScreen.tsx
@@ -1,10 +1,11 @@
 import { AntDesign } from '@expo/vector-icons';
 import React from 'react';
 import { useState, useEffect } from 'react'
-import { Button, View, ScrollView, StyleSheet, TextInput, Pressable, Text} from 'react-native';
+import { View, StyleSheet, TextInput, Pressable, Text} from 'react-native';
 import { auth } from '../../../utils/firebaseConfig';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { addNewUserWithID } from '../../../utils/services/usersFunctions';
+import { useTheme } from '../../../utils/contexts/ThemeContext';
 
 export function RegisterScreen({ navigation, route }: any) {
   const [usernameInput, setUsernameInput] = useState(''); 
@@ -36,57 +37,138 @@ export function RegisterScreen({ navigation, route }: any) {
     }
   }
 
+  const {theme} = useTheme();
+  const registerStyle = StyleSheet.create({
+    mainContainer: {
+      width: "100%",
+      height: "100%",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: theme.colors.backgroundColor
+    },
+    loginContainer: {
+      width:'90%',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#FFFFFF',
+      borderWidth: 1,
+      borderRightWidth: 4,
+      borderBottomWidth: 4,
+      borderRadius: 20,
+      borderColor: theme.colors.border,
+    },
+    loginTitle: {
+      fontSize: 24,
+      fontWeight: "bold",
+      marginTop: 40,
+      marginBottom: 15,
+    },
+    loginInput: {
+      width: '80%',
+      fontSize: 16,
+      borderBottomWidth: 1,
+      borderStyle: 'solid',
+      borderBottomColor: 'black',
+      marginTop: 25,
+    },
+    loginForgotContainer: {
+      width: '80%',
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      fontSize: 11,
+      marginTop: 10,
+      marginBottom: 30,
+    },
+    loginForgotText: {
+      textDecorationLine: 'underline',
+      fontSize: 11,
+    },
+    loginRegisterContainer: {
+      width: '100%',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 40,
+    },
+    loginRegisterText: {
+      fontSize: 13,
+    },
+    loginRegisterLink: {
+      textDecorationLine: 'underline',
+      fontSize: 13,
+      fontWeight: "bold",
+    },
+    loginSignInBtn: {
+      marginBottom: 40,
+      fontSize: 16,
+      fontWeight: "bold",
+      color: '#FFFFFF',
+      backgroundColor: 'black',
+      paddingHorizontal: 70,
+      paddingVertical: 15, 
+      borderRadius: 11,
+    },
+  })
+
   return (
-    <View style={{paddingHorizontal: 20}}>
-      <Text> REGISTER </Text>
+    <View style={registerStyle.mainContainer}>
+      <View style={registerStyle.loginContainer}>
+        <Text style={registerStyle.loginTitle}>Sign Up</Text>
 
-      {(error !== "") && <Text>{error}</Text>}
+        {(error !== "") && <Text>{error}</Text>}
 
-      {/* Custom Login */}
-      <TextInput
-        style={styles.input}
+        {/* Custom Login */}
+        <TextInput
+        style={registerStyle.loginInput}
         onChangeText={setUsernameInput}
-        placeholder='username'
+        placeholder='Name'
         placeholderTextColor="#D3D3D3" 
         value={usernameInput}
       />
+
       <TextInput
-        style={styles.input}
+        style={registerStyle.loginInput}
         onChangeText={setEmailInput}
-        placeholder='email'
+        placeholder='Username'
         placeholderTextColor="#D3D3D3" 
         value={emailInput}
       />
-      <TextInput
+
+     <TextInput
         secureTextEntry={true}
-        style={styles.input}
+        style={registerStyle.loginInput}
         onChangeText={setPasswordInput}
-        placeholder='password'
+        placeholder='Password'
         placeholderTextColor="#D3D3D3" 
         value={passwordInput}
       />
       <TextInput
         secureTextEntry={true}
-        style={styles.input}
+        style={registerStyle.loginInput}
         onChangeText={setConfirmPasswordInput}
-        placeholder='confirm your password'
+        placeholder='Confirm Password'
         placeholderTextColor="#D3D3D3" 
         value={confirmPasswordInput}
       />
 
-      <Button
-        title="Register"
-        onPress={handleSubmitForRegister}
-      />
+      <View style={registerStyle.loginForgotContainer}>
+        <Pressable >
+          <Text style={registerStyle.loginForgotText}>Forgot Password</Text>
+        </Pressable>
+      </View>
+
+        {/* Custom Register */}
+        <Pressable style={registerStyle.loginRegisterContainer} onPress={() => navigation.navigate('LoginScreen')}>
+            <Text style={registerStyle.loginRegisterText}>Have an account?&nbsp;</Text>
+            <Text style={registerStyle.loginRegisterLink}>Login Here</Text>
+        </Pressable>
+
+        <Pressable onPress={handleSubmitForRegister}>
+          <Text style={registerStyle.loginSignInBtn}>Register</Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  input: {
-    height: 40, 
-    borderColor: 'gray', 
-    borderWidth: 1,
-    backgroundColor: "white"
-  }
-})
