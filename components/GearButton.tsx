d1e5325bb60f19a93501da3225a96dc9eb789555
Junyi Huang <jyiyi@bu.edu>
2024-04-21 23:55:58 -0400

    move to branch based off of main

--- components/GearButton.tsx
+++ components/GearButton.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useTheme } from '../utils/contexts/ThemeContext';
 import { MaterialCommunityIcons, Octicons } from "@expo/vector-icons";
 import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Modalize } from 'react-native-modalize';
+import GearModal from './GearModal';
 
-const GearButton = () => {
+interface GearButtonProps {
+  openModal: () => void;
+}
+
+const GearButton: React.FC<GearButtonProps> = ({ openModal }) => {
   const { toggleTheme } = useTheme();
+  const modalizeRef = useRef<Modalize>(null); 
 
   const {theme} = useTheme();
   const styles = StyleSheet.create(
@@ -12,22 +19,23 @@ const GearButton = () => {
     position: 'relative',
     padding: 8,
     marginLeft: 4,
+  },
+});
 
-  }}
-)
+const onOpen = () => {
+  modalizeRef.current?.open();
+};
 
   // Return the button element
   return (
-    <View style={styles.gearButtonContainer}>
-      <TouchableOpacity onPress={toggleTheme}>    
-        <Octicons name="gear" size={28} color={theme.colors.gearButton}  />
-        {/* <MaterialCommunityIcons name="theme-light-dark" size={28} color={theme.colors.gearButton} /> */}
+    <><View style={styles.gearButtonContainer}>
+      <TouchableOpacity onPress={openModal}>
+        <Octicons name="gear" size={28} color={theme.colors.gearButton} />
       </TouchableOpacity>
     </View>
-      
+    <GearModal ref={modalizeRef} />
+    </> 
   );
 };
-
-
 
 export default GearButton;
